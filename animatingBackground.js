1f23846c7f593d025efe8ff8bf6d98c6d9c660d4
ok-webdesign <me@omar-kamal.com>
2019-12-30 10:07:41 -0800

    added button to start

--- animatingBackground.js
+++ animatingBackground.js
@@ -1,4 +1,4 @@
-const numDots = 50;
+const numDots = 60;
 const screenHeight = window.screen.availHeight;
 const screenWidth = window.screen.availWidth;
 const clientheight = document.documentElement.clientHeight;
@@ -11,29 +11,29 @@ function createNDots (n) {
     return dots;
 }
 
-function glow () {
-
-}
-
 $(document).ready(function () {
 
     var dots = createNDots(numDots);
+    var hasBeenRun = false;
 
     $('.container').append(dots);
-
-    for (let i = 0; i < numDots; i++) {
-        var aniHeight = Math.max(5, Math.floor(Math.random() * 30));
-        var aniWidth = aniHeight;
-        $(`#dot-${i}`).animate({
-            top: `${Math.floor(Math.random() * clientheight)}px`,
-            bottom: `${Math.floor(Math.random() * clientheight)}px`,
-            left: `${Math.floor(Math.random() * screenWidth)}px`,
-            right: `${Math.floor(Math.random() * screenWidth)}px`,
-            height: aniHeight,
-            width: aniWidth,
-            opacity: Math.random()
-        }, 2000);
-    }
+    
+    $('.starter').click(function() {
+        $('.dot').css('visibility', 'visible');
+        for (let i = 0; i < numDots; i++) {
+            var aniHeight = Math.max(5, Math.floor(Math.random() * 30));
+            var aniWidth = aniHeight;
+            $(`#dot-${i}`).animate({
+                top: `${Math.floor(Math.random() * clientheight)}px`,
+                bottom: `${Math.floor(Math.random() * clientheight)}px`,
+                left: `${Math.floor(Math.random() * screenWidth)}px`,
+                right: `${Math.floor(Math.random() * screenWidth)}px`,
+                height: aniHeight,
+                width: aniWidth,
+                opacity: Math.random()
+            }, 2000);
+        }
+    })
 
 });
 
