8c4f3c73d7c0368fb20d2bf92c1cee7598baecb3
ok-webdesign <me@omar-kamal.com>
2019-12-31 12:28:04 -0800

    Update animatingBackground.js

--- animatingBackground.js
+++ animatingBackground.js
@@ -1,21 +1,31 @@
-const numDots = 60;
+const numDots = 20;
 const screenHeight = window.screen.availHeight;
 const screenWidth = window.screen.availWidth;
 const clientheight = document.documentElement.clientHeight;
 const clientWidth = document.documentElement.clientWidth;
 
 function createNDots (n) {
+
     var dots = '';
+
     for (let i = 0; i < n; i++) {
+
         dots += `<div id=dot-${i} class="dot"></div>`;
+
     }
+
     return dots;
+
 }
 
+
 function setUpDotPositions () {
+
     for (let i = 0; i < numDots; i++) {
+
         var aniHeight = Math.max(5, Math.floor(Math.random() * 30));
         var aniWidth = aniHeight;
+
         $(`#dot-${i}`).animate({
             top: `${Math.floor(Math.random() * clientheight)}px`,
             bottom: `${Math.floor(Math.random() * clientheight)}px`,
@@ -25,12 +35,17 @@ function setUpDotPositions () {
             width: aniWidth,
             opacity: Math.random()
         }, 2000);
+
     }
+
 }
 
+
 $(document).ready(function () {
+
     var dots = createNDots(numDots);
     var hasBeenRun = false;
+
     $('.container').append(dots);
     $('.starter').click(function() {
         if (!hasBeenRun) {
@@ -39,5 +54,8 @@ $(document).ready(function () {
         }
         setUpDotPositions();
     });
+
+    // setInterval(setUpDotPositions, 30);
+
 });
 
